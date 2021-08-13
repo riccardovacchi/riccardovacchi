0b1b0bf0d43b6eaaee4f718804480edb46bfd9f0
rvacchi <rvacchi@minsait.com>
2021-08-13 15:40:36 +0200

    fine tutorial

--- SAMPLE/REACT/fullstack-sushi/src/components/card.js
+++ SAMPLE/REACT/fullstack-sushi/src/components/card.js
@@ -5,11 +5,12 @@ class Card extends Component {
         return (
             <div className="col">
                 <div className="card" style={{ width: '18rem', textAlign: 'center' }}>
+                    <button onClick={() => this.props.onIncrement(this.props.card)} className="btn btn-primary">Aggiungi <span className='badge badge-light'>{this.props.card.quantità}</span></button>
                     <img src={this.props.card.immagine} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{this.props.card.nome} Roll</h5>
                         <p className="card-text">€{this.props.card.prezzo}</p>
-                        <button onClick={()=>this.props.onDelete(this.props.card.id)} className="btn btn-outline-danger">Elimina</button>
+                        <button onClick={() => this.props.onDelete(this.props.card.id)} className="btn btn-outline-danger">Elimina</button>
                     </div>
                 </div>
             </div>
