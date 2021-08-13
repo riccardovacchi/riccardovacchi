6b426354ab5266a5044af6a6b44eaf3d9fadc6f4
rvacchi <rvacchi@minsait.com>
2021-08-13 19:52:30 +0200

    creato superuser

--- SAMPLE/DJANGO/fullstack_sushi/rolls/models.py
+++ SAMPLE/DJANGO/fullstack_sushi/rolls/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Rolls(models.Model):
+    immagine = models.ImageField()
+    nome = models.CharField(max_length=20)
+    prezzo = models.FloatField()
+    
