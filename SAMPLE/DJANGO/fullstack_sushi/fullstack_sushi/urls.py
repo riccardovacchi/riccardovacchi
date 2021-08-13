c3b2a180c855ba470fa5cd8b7c201d1d248887f7
rvacchi <rvacchi@minsait.com>
2021-08-13 19:22:14 +0200

    add views rolls

--- SAMPLE/DJANGO/fullstack_sushi/fullstack_sushi/urls.py
+++ SAMPLE/DJANGO/fullstack_sushi/fullstack_sushi/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rolls import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('rolls/', views.rolls),
 ]
