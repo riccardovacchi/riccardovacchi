2d17911e972b0d213d4c6d8c0a6ec58d2422a0df
rvacchi <rvacchi@minsait.com>
2021-08-14 13:29:02 +0200

    add MEDIA_ROOT

--- SAMPLE/DJANGO/fullstack_sushi/rolls/models.py
+++ SAMPLE/DJANGO/fullstack_sushi/rolls/models.py
@@ -5,3 +5,6 @@ class Roll(models.Model):
     immagine = models.ImageField()
     nome = models.CharField(max_length=20)
     prezzo = models.FloatField()
+
+    def __str__(self):
+        return self.nome
