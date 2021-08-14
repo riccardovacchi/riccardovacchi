68e6984a3ebe8203bbb6d305ed4bb1e0e0eea092
rvacchi <rvacchi@minsait.com>
2021-08-14 13:12:06 +0200

    creata tabella Rolls

--- SAMPLE/DJANGO/fullstack_sushi/rolls/models.py
+++ SAMPLE/DJANGO/fullstack_sushi/rolls/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Rolls(models.Model):
+class Roll(models.Model):
     immagine = models.ImageField()
     nome = models.CharField(max_length=20)
     prezzo = models.FloatField()
-    
